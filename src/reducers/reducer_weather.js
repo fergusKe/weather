5eaf6ca6904412791452a596b9119d318612db4d
fergus <fergus.jingtai@gmail.com>
2017-02-06 03:07:09 +0800

    61. Building a List Container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,7 @@ export default function(state = [], action) {
   switch (action.type) {
   case FETCH_WEATHER:
     // return state.concat([action.paload.data]);
-    return [ action.payload.sata, ...state ]; // [ city, city, city ] not [city, [ city, city ] ]
+    return [ action.payload.data, ...state ]; // [ city, city, city ] not [city, [ city, city ] ]
   }
   return state;
 }
