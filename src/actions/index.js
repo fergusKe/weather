857b32b61fa96bc36d43a989ea265adcc2f2f2ad
fergus <fergus.jingtai@gmail.com>
2017-02-05 11:56:15 +0800

    57. Ajax Requests with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '810e2f26e95f22b6d7649b98541a9bc1';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
